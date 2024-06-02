e983815572bcae9d1d9a396eec76e6d6d44c77c6
fikrim1109 <88815201+fikrim1109@users.noreply.github.com>
2024-06-02 22:48:39 +0800

    feat : add forum js

--- server.js
+++ server.js
@@ -6,13 +6,20 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Import forum routes
+const forumRoutes = require('./routes/forum');
+
 app.use(bodyParser.json());
 app.use(morgan('dev'));
 app.use(cors());
 
+// Main route
 app.get('/', (req, res) => {
   res.send('Welcome to Signora API');
 });
+
+// Forum routes
+app.use('/api/forum', forumRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
